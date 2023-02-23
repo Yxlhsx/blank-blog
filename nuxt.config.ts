5e7cfd1935a4ea4bf7d70b2c9ecf223895dbc345
Yxlhsx <yxlhsx@qq.com>
2023-02-23 22:48:00 +0800

    修改配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     app: {
-        // baseURL: '/blank-blog',
+        baseURL: '/blank-blog',
         head: { title: '空白博客' }
     },
     modules: ['@nuxt/content'],
